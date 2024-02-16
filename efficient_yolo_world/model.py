e3246b7ceaebdd79339bc97bb90ed3402b5b4159
James Gallagher <jamesg@jamesg.blog>
2024-02-16 20:53:49 +0000

    add model code

---
+++ efficient_yolo_world/model.py
@@ -0,0 +1,44 @@
+import os
+from dataclasses import dataclass
+
+import torch
+
+import supervision as sv
+from autodistill.utils import load_image
+from autodistill.detection import CaptionOntology, DetectionBaseModel
+from autodistill_yoloworld import YOLOWorldModel
+from autodistill_efficientsam import EfficientSAM
+import numpy as np
+
+HOME = os.path.expanduser("~")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+@dataclass
+class EfficientYOLOWorld(DetectionBaseModel):
+    ontology: CaptionOntology
+    
+    def __init__(self, ontology: CaptionOntology):
+        self.detection_model = YOLOWorldModel(ontology=ontology)
+        self.segmentation_model = EfficientSAM(None)
+
+    def predict(self, input: str, confidence: int = 0.5) -> sv.Detections:
+        image = load_image(input)
+
+        result = self.detection_model.predict("bookshelf.jpeg", confidence=0.1).with_nms()
+
+        result.mask = np.array([None] * len(result.xyxy))
+
+        for i, [x_min, y_min, x_max, y_max] in enumerate(result.xyxy):
+            print(x_min, y_min, x_max, y_max)
+
+            y_min, y_max = int(y_min), int(y_max)
+            x_min, x_max = int(x_min), int(x_max)
+            input_image = image[y_min:y_max, x_min:x_max]
+            mask = self.segmentation_model.predict(input_image)
+            full_mask = np.zeros((image.shape[0], image.shape[1]))
+
+            full_mask[y_min:y_max, x_min:x_max] = mask.mask[0]
+            result.mask[i] = full_mask.astype(bool)
+
+        return result
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+from setuptools import find_packages
+import re
+
+with open("./autodistill_efficient_yolo_world/__init__.py", 'r') as f:
+    content = f.read()
+    # from https://www.py4u.net/discuss/139845
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autodistill-efficient-yolo-world",
+    version=version,
+    author="Roboflow",
+    author_email="support@roboflow.com",
+    description="EfficientSAM + YOLO-World base model for use with Autodistill",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roboflow/autodistill-efficient-yolo-world",
+    install_requires=[
+        "supervision",
+        "autodistill-yolo-world",
+        "autodistill-efficientsam",
+        "numpy",
+        "torch"
+    ],
+    packages=find_packages(exclude=("tests",)),
+    extras_require={
+        "dev": ["flake8", "black==22.3.0", "isort", "twine", "pytest", "wheel"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
